0a478b01df46a2a5fe851f8ce5a71d932b7205be
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:44:58 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -69,6 +69,9 @@ if ( ! function_exists( 'dashscroll_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'dashscroll-small', 200, 200, true );
 		add_image_size( 'dashscroll-medium', 520, 293, true );
